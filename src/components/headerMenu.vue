7432ceface760fe70f5a442acb4fe0c4cfd911d3
ning9010 <zengning1002@gmail.com>
2024-03-08 15:59:37 +0800

    布局 添加baidu地图

---
+++ src/components/headerMenu.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="headermenu">
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+            <el-menu-item index="1">Processing Center</el-menu-item>
+            <el-sub-menu index="2">
+                <template #title>Workspace</template>
+                <el-menu-item index="2-1">item one</el-menu-item>
+                <el-menu-item index="2-2">item two</el-menu-item>
+                <el-menu-item index="2-3">item three</el-menu-item>
+                <el-sub-menu index="2-4">
+                    <template #title>item four</template>
+                    <el-menu-item index="2-4-1">item one</el-menu-item>
+                    <el-menu-item index="2-4-2">item two</el-menu-item>
+                    <el-menu-item index="2-4-3">item three</el-menu-item>
+                </el-sub-menu>
+            </el-sub-menu>
+            <el-menu-item index="3" disabled>Info</el-menu-item>
+            <el-menu-item index="4">Orders</el-menu-item>
+        </el-menu>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const activeIndex = ref('1')
+const handleSelect = (key, keyPath) => {
+    console.log(key, keyPath)
+}
+</script>
+<style scoped>
+.el-menu-demo {
+    width: 100vw;
+    height: 6vh;
+    margin-bottom: 1vh;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,24 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+import BaiduMap from 'vue3-baidu-map-gl';
 
 const app = createApp(App)
+app.use(ElementPlus)
+app.use(BaiduMap, {
+    ak: 'voMP8EuTAM5gMjpN1qVHGn3sCfUGahEo',
+})
 
-app.use(router)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+// app.use(router)
 
 app.mount('#app')
